76bd61bc967ef22a232b911a0e14fe89b2b06168
Peylics <lisichen_2901@outlook.com>
2023-10-30 23:29:50 +0800

    Added CORS in backend

--- api/App/exts.py
+++ api/App/exts.py
@@ -1,12 +1,14 @@
 # Function: Initialize extensions
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 db = SQLAlchemy()  # ORM
 migrate = Migrate()  # data migration
+cors = CORS()  # Cross-origin request
 
 
 def init_exts(app):
     db.init_app(app=app)
     migrate.init_app(app=app, db=db)
-
+    cors.init_app(app, origins="*")  
